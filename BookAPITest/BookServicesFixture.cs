8e7a087aedac1e014f9642f4ddd4aa52d3a330fb
tavisca-akadam <ankadam@tavisca.com>
2019-09-05 22:43:15 +0530

    Updated Testcases for PUT and DELETE

--- BookAPITest/BookServicesFixture.cs
+++ BookAPITest/BookServicesFixture.cs
@@ -158,5 +158,155 @@ namespace BookAPITest
                 Category = "@#1"
             }));
         }
+
+        [Fact]
+        public void Update_book_using_put_with_valid_parameter()
+        {
+            var bookStore = _bookService.GetBookStore();
+            _bookService.Post(new Book
+            {
+                ID = 2,
+                Name = "PQRs",
+                Price = 200,
+                Author = "GSanap",
+                Category = "AAA"
+            });
+
+            _bookService.Put(2, new Book
+            {
+                Name = "PQRS",
+                Price = 200,
+                Author = "GSANAP",
+                Category = "AAA"
+            });
+            string expectedResult = "PQRS";
+            var actualResult = _bookService.Get(2);
+            Assert.Equal(expectedResult, actualResult.Name);
+        }
+
+        [Fact]
+        public void Update_book_using_put_with_invalid_name()
+        {
+            var bookStore = _bookService.GetBookStore();
+            _bookService.Post(new Book
+            {
+                ID = 2,
+                Name = "PQRs",
+                Price = 200,
+                Author = "GSanap",
+                Category = "AAA"
+            });
+
+            Assert.Throws<InvalidParameterException>(() => _bookService.Put(2, new Book
+            {
+                Name = "PQRS@!#",
+                Price = 200,
+                Author = "GSANAP",
+                Category = "AAA"
+            }));
+        }
+
+        [Fact]
+        public void Update_book_using_put_with_invalid_price()
+        {
+            var bookStore = _bookService.GetBookStore();
+            _bookService.Post(new Book
+            {
+                ID = 2,
+                Name = "PQRs",
+                Price = 200,
+                Author = "GSanap",
+                Category = "AAA"
+            });
+
+            Assert.Throws<InvalidParameterException>(() => _bookService.Put(2, new Book
+            {
+                Name = "PQRS",
+                Price = 0,
+                Author = "GSANAP",
+                Category = "AAA"
+            }));
+        }
+
+        [Fact]
+        public void Update_book_using_put_with_invalid_author()
+        {
+            var bookStore = _bookService.GetBookStore();
+            _bookService.Post(new Book
+            {
+                ID = 2,
+                Name = "PQRs",
+                Price = 200,
+                Author = "GSanap",
+                Category = "AAA"
+            });
+
+            Assert.Throws<InvalidParameterException>(() => _bookService.Put(2, new Book
+            {
+                Name = "PQRS",
+                Price = 200,
+                Author = "#GSANAP",
+                Category = "AAA"
+            }));
+        }
+
+
+        [Fact]
+        public void Update_book_using_put_with_invalid_category()
+        {
+            var bookStore = _bookService.GetBookStore();
+            _bookService.Post(new Book
+            {
+                ID = 2,
+                Name = "PQRs",
+                Price = 200,
+                Author = "GSanap",
+                Category = "AAA"
+            });
+
+            Assert.Throws<InvalidParameterException>(() => _bookService.Put(2, new Book
+            {
+                Name = "PQRS",
+                Price = 200,
+                Author = "GSANAP",
+                Category = "$#@!"
+            }));
+        }
+
+        [Theory]
+        [InlineData(2)]
+        public void Delete_book_using_delete_with_valid_id(int id)
+        {
+            var bookStore = _bookService.GetBookStore();
+            _bookService.Post(new Book
+            {
+                ID = id,
+                Name = "PQRs",
+                Price = 200,
+                Author = "GSanap",
+                Category = "AAA"
+            });
+
+            _bookService.Delete(id);
+
+            Assert.Throws<BookNotFoundException>(() => _bookService.Get(id));
+        }
+
+        [Theory]
+        [InlineData(3)]
+        public void Delete_book_using_delete_with_invalid_id(int id)
+        {
+            var bookStore = _bookService.GetBookStore();
+            _bookService.Post(new Book
+            {
+                ID = 2,
+                Name = "PQRs",
+                Price = 200,
+                Author = "GSanap",
+                Category = "AAA"
+            });
+            
+            Assert.Throws<BookNotFoundException>(() => _bookService.Delete(3));
+        }
     }
 }
